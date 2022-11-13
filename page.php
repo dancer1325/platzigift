83d8a1eec8ffc5bbe99a83a78e7a12950d36969c
dancer1325 <alfredotic0809@gmail.com>
2022-11-13 16:38:05 +0100

    refactor(Section3, Chapter3): add commentaries

--- page.php
+++ page.php
@@ -1,15 +1,19 @@
-<?php get_header(); ?>
+<!-- Get either header or footer, as it was done in index.php -->
+<?php get_header(); ?> <!-- Load header template https://developer.wordpress.org/reference/functions/get_header/  -->
 
+<!-- Bring our page's content -->
+<!-- https://html.spec.whatwg.org/#hierarchically-correct-main-element -->
 <main class='container'>
-    <?php if(have_posts()){
+    <?php if(have_posts()){     // https://developer.wordpress.org/reference/functions/have_posts/
             while(have_posts()){
-                the_post(); ?>
-            <h1 class='my-3'><?php the_title(); ?></h1>
+                the_post(); ?>  <!-- https://developer.wordpress.org/reference/functions/the_post/ -->
+            <h1 class='my-3'><?php the_title(); ?></h1> <!-- https://developer.wordpress.org/reference/functions/the_title/ -->
+            <!-- my-3 notation https://getbootstrap.com/docs/5.0/utilities/spacing/#notation -->
 
-            <?php the_content(); ?>
+            <?php the_content(); ?> <!-- https://developer.wordpress.org/reference/functions/the_content/ -->
 
          <?php }
     }?>
 </main>
 
-<?php get_footer(); ?>
+<?php get_footer(); ?>  <!-- Load footer template  https://developer.wordpress.org/reference/functions/get_footer/ -->
