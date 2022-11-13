b33c52bcc49e1dd89be8f2a0b2e62b0b138f8cbd
dancer1325 <alfredotic0809@gmail.com>
2022-11-13 16:56:13 +0100

    refactor(Section3, Chapter4): add commentaries

--- single.php
+++ single.php
@@ -1,17 +1,20 @@
-<?php get_header(); ?>
+<?php get_header(); ?>  <!-- Load header template https://developer.wordpress.org/reference/functions/get_header/  -->
 
+<!-- Bring our page's content -->
+<!-- https://html.spec.whatwg.org/#hierarchically-correct-main-element -->
 <main class='container my-3'>
-    <?php if(have_posts()){
+    <?php if(have_posts()){     // https://developer.wordpress.org/reference/functions/have_posts/
             while(have_posts()){
-                the_post();
+                the_post();     // https://developer.wordpress.org/reference/functions/the_post/
             ?>
-                <h1 class='my-5'><?php the_title() ?></h1>
+                <h1 class='my-5'><?php the_title() ?></h1>  <!-- https://developer.wordpress.org/reference/functions/the_title/ -->
+                <!-- my-5 notation https://getbootstrap.com/docs/5.0/utilities/spacing/#notation -->
                 <div class="row">
                     <div class="col-6">
-                        <?php the_post_thumbnail('large'); ?>
+                        <?php the_post_thumbnail('large'); ?>   <!-- https://developer.wordpress.org/reference/functions/the_post_thumbnail/ -->
                     </div>
                     <div class="col-6">
-                        <?php the_content(); ?>
+                        <?php the_content(); ?> <!-- https://developer.wordpress.org/reference/functions/the_content/ -->
                     </div> 
                 </div>
             <?php
@@ -19,4 +22,4 @@
     } ?>
 
 </main>
-<?php get_footer(); ?>
+<?php get_footer(); ?>  <!-- Load footer template  https://developer.wordpress.org/reference/functions/get_footer/ -->
